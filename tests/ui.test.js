e30d2e393d6af6e079dd4330a25294337bfb5b22
Kristian Hristov <kristian_hristov@outlook.com>
2024-03-07 20:07:00 +0200

    add two more tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,7 +1,10 @@
 const { expect, test} = require('@playwright/test')
 
+const baseURL = "http://localhost:3000"
+
+
 test ("Verify all books link is visible", async({page}) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const allBooksLink = await page.$('a[href="/catalog"]')
     const isAllBooksVisible = await allBooksLink.isVisible()
@@ -9,7 +12,7 @@ test ("Verify all books link is visible", async({page}) => {
 }) 
 
 test ("Verify login button is visible", async({page}) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const loginButton = await page.$('a[href="/login"]')
     const isLoginButtonVisible = await loginButton.isVisible()
@@ -18,9 +21,32 @@ test ("Verify login button is visible", async({page}) => {
 
 
 test ("Verify register button is visible", async({page}) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const registerButton = await page.$('a[href="/register"]')
     const isRegisterButtonVisible = await registerButton.isVisible()
     expect(isRegisterButtonVisible).toBe(true);
+}) 
+
+
+test.only ("Verify all books link is visible after user is logged in", async({page}) => {
+    await page.goto(baseURL);
+    await page.waitForSelector("nav.navbar");
+
+    await page.click('a[href="/login"]')
+    await page.fill('#email', "peter@abv.bg")
+    await page.fill('#password', "123456")
+    await page.click('#login-form > fieldset > input')
+
+    
+     //check if logout button is visible
+    const logoutButton = await page.$('#logoutBtn')
+    const isLogoutButtonsVisible = await logoutButton.isVisible()
+    expect(isLogoutButtonsVisible).toBe(true);
+
+
+    //check if all books link is visible
+    const allBooksLink = await page.$('a[href="/catalog"]')
+    const isAllBooksVisible = await allBooksLink.isVisible()
+    expect(isAllBooksVisible).toBe(true);
 }) 
