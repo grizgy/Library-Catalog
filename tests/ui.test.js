b07771e0a165026957d67652cd71a443c3f867a6
Kristian Hristov <kristian_hristov@outlook.com>
2024-03-07 19:46:34 +0200

    add two more test for login and register

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -7,3 +7,20 @@ test ("Verify all books link is visible", async({page}) => {
     const isAllBooksVisible = await allBooksLink.isVisible()
     expect(isAllBooksVisible).toBe(true);
 }) 
+
+test ("Verify login button is visible", async({page}) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const loginButton = await page.$('a[href="/login"]')
+    const isLoginButtonVisible = await loginButton.isVisible()
+    expect(isLoginButtonVisible).toBe(true);
+}) 
+
+
+test ("Verify register button is visible", async({page}) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const registerButton = await page.$('a[href="/register"]')
+    const isRegisterButtonVisible = await registerButton.isVisible()
+    expect(isRegisterButtonVisible).toBe(true);
+}) 
